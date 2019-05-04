8c8fcc3b00954149ecacb527055ba5839e83afff
Helene <1810738627@stud.fh-kufstein.ac.at>
2019-05-04 14:14:16 +0200

    correction

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open('fxcalc-v1')
         .then(cache => cache.addAll([
-            '/fx-calculator/',
+            '/mobile_fx_calculator/',
             'index.html',
             'style.css',
             'app.js',
